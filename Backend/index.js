53248574d193ffcaa109d324fc3ed1aef8ce14ab
Tejas <tejasspatell2@gmail.com>
2025-09-12 19:00:03 +0530

    fix error

--- Backend/index.js
+++ Backend/index.js
@@ -42,9 +42,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 1000 * 60 * 60 * 24,
+      secure: true,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   })
 );

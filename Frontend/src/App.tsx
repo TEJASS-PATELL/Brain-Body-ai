96ba07e62a6f71d20eb50f63aa31ea0079a2fb45
Tejas <tejasspatell2@gmail.com>
2026-05-18 17:21:10 +0530

    Building DocuMind

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import api from './api';
 
 function App() {
+
+  useEffect(() => {
+    api.get('/api/auth/userinfo')
+      .then(res => {
+        localStorage.setItem('username', res.data.name);
+        localStorage.setItem('email', res.data.email);
+      })
+      .catch(err => console.error('Failed to fetch user info:', err));
+  }, []);
+
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <Outlet /> 
+      <Outlet />
     </>
   );
 }
